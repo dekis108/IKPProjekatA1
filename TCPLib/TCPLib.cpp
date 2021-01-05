c8c2aef3f2399351fac54846dc754d222eb0a7d1
dejan.subasic <dejan.subasic@gmail.com>
2021-01-05 14:56:51 +0100

    Defined projects

---
+++ TCPLib/TCPLib.cpp
@@ -0,0 +1,10 @@
+// TCPLib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnTCPLib()
+{
+}
