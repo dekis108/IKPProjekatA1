adbded4ca8e58b891fedb0a07bac847313f4218c
Dragana Trivunovic <dragana.trivunovic@vega.local@git config --global user.email dragana.trivunovic@vega.local>
2021-01-10 18:32:59 +0100

    Publisher connects to PubSubService and sends a test msg

--- Publisher/Publisher.cpp
+++ Publisher/Publisher.cpp
@@ -1,20 +1,128 @@
-// Publisher.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
-#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+#include "../Common/Measurment.h";
+
+#pragma comment(lib,"WS2_32")
+
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT 27016
+#define ADDRESS "127.0.0.1"
+#define TYPE_STRING_LENGHT 10
+#define DEMOTESTCOUNT 20
+
+int Init();
+bool InitializeWindowsSockets();
+bool CreateSocket();
+bool Connect();
+bool Publish();
+
+SOCKET connectSocket = INVALID_SOCKET;
+sockaddr_in serverAddress;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int result = Init();
+    if (result) {
+        printf("ERROR CODE %d, press any key to exit\n", result);
+        getchar();
+        return result;
+    }
+
+    printf("Client initialised. Press enter publish\n");
+    getchar();
+    printf("Sending...\n");
+
+    if (Publish()) {
+        printf("Done, stopping..\n");
+    }
+    else {
+        printf("An error occured\n");
+    }
+
+    getchar();
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool  Publish() {
+    Measurment* msg = (Measurment*)malloc(sizeof(Measurment));
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    //TODO: generisanje vrednosti
+    msg->value = 13;
+    strcpy(msg->topic,"test1");
+    strcpy(msg->type,"test2");
+
+    int iResult = send(connectSocket, (const char*)msg, sizeof(Measurment), 0);
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        return false;
+    }
+    //Sleep(10);
+    return true;
+}
+
+int Init() {
+
+    if (InitializeWindowsSockets() == false)
+    {
+        return 1;
+    }
+    if (CreateSocket() == false) {
+        return 2;
+    }
+    if (Connect() == false) {
+        return 3;
+    }
+
+    return 0;
+}
+
+bool Connect() {
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr(ADDRESS);
+    serverAddress.sin_port = htons(DEFAULT_PORT);
+
+    if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        printf("Unable to connect to server.\n");
+        closesocket(connectSocket);
+        WSACleanup();
+        return false;
+    }
+    return true;
+}
+
+bool CreateSocket() {
+    connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (connectSocket == INVALID_SOCKET)
+    {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return false;
+    }
+    return true;
+}
+
+bool InitializeWindowsSockets() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+        return false;
+    }
+    return true;
+}
