a08ac4cac883def981f9264b4091ca963f0d40c8
dejan.subasic <dejan.subasic@gmail.com>
2021-01-12 17:04:29 +0100

    Visual studio acting up

--- Common/Measurment.h
+++ Common/Measurment.h
@@ -17,3 +17,33 @@ typedef struct _msgFormat {
     int value;
 
 }Measurment;
+
+
+//VISUAL STUDIO MI NE DA DISEM
+/*
+char* GetStringFromEnumHelper(MeasurmentTopic topic) {
+	//const char* strings[] = { "Analog", "Status" };
+	//return "Deja";
+	//return strings[topic];
+}
+
+*/
+
+/*
+char* GetStringFromEnumHelper(MeasurmentType type) {
+	switch (type)
+	{
+	case SWG:
+		char returnValue1[] = "SWG";
+		return returnValue1;
+	case CRB:
+		char returnValue2[] = "CRB";
+		return returnValue2;
+	case MER:
+		char returnValue3[] = "MER";
+		return returnValue3;
+	default:
+		char returnValue4[] = "StringFromEnumHelper not updated for this value";
+		return returnValue4;
+	}
+}*/
